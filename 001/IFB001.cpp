a101863d3ae31850d0170dda7fef5510f76c27f5
AlanPrates <alan_nz@live.com>
2021-11-09 00:28:17 -0300

    Create IFB001.cpp

---
+++ 001/IFB001.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nota;
+    cin >> nota;
+
+    setlocale(LC_ALL, "Portuguese");
+    cout << "Alan Prates, sua nota é " << nota;
+
+    return 0;
+}
