b362c87ca0488106513a7f54f1dc3ea4d5cfff65
AlanPrates <alan_nz@live.com>
2021-12-02 08:12:09 -0300

    ILP AENPE 2021 A1 Estruturas de Controle

---
+++ 05/STI 11 ILP.cpp
@@ -0,0 +1,22 @@
+// STI 11 ILP.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+// 
+//     1     1     1        3 
+// S= ___ + ___ + ___ : S= ___ (Decimal S= 0,3)
+//    2.3 x 3.4 x 4.5       10 
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float S1, S2;
+    S1 = 3;
+    S2 = 10;
+
+    cout << " O valor de S = " << S1 / S2;
+
+    return 0;
+}
+
+
